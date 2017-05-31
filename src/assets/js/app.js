ecf0ff1976faf17b4d9d2d2078c65800ea2ff1ca
Raúl <Raul@samsung-pc.com>
2017-05-31 17:56:44 +0200

    Updated css transitions

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,3 +2,14 @@
 particlesJS.load('particles-js', 'src/assets/particlesjs-config.json', function() {
   console.log('Particles.js config loaded');
 });
+ 
+$(document).ready(function(){
+  // console.log("-->" + $myElem.offset().top);
+  $avatar = $('#avatar');
+  $avatarInfo = $("div[id^='av-']");
+   $(document).scroll(function(){
+    //  console.log("scroll");
+     $avatar.toggleClass('avatar-min', $(this).scrollTop() > 1);
+     $avatarInfo.toggleClass('avatar-off', $(this).scrollTop() > 1);
+ });
+});
