d15efd52ff27961d4f3d2d9f848b4197f8ae67c8
Raúl <Raul@samsung-pc.com>
2017-06-01 13:33:42 +0200

    Updated projects definitions

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,15 +1,17 @@
+/*** Custom Javascript ***/ 
+
+// Load Particles library
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'src/assets/particlesjs-config.json', function() {
+particlesJS.load('particles-js', 'src/assets/particlesjs-config.json', function () {
   console.log('Particles.js config loaded');
 });
- 
-$(document).ready(function(){
-  // console.log("-->" + $myElem.offset().top);
+
+// Set Avatar style/position on window scroll
+$(document).ready(function () {
   $avatar = $('#avatar');
   $avatarInfo = $("div[id^='av-']");
-   $(document).scroll(function(){
-    //  console.log("scroll");
-     $avatar.toggleClass('avatar-min', $(this).scrollTop() > 1);
-     $avatarInfo.toggleClass('avatar-off', $(this).scrollTop() > 1);
- });
+  $(document).scroll(function () {
+    $avatar.toggleClass('avatar-min', $(this).scrollTop() > 1);
+    $avatarInfo.toggleClass('avatar-off', $(this).scrollTop() > 1);
+  });
 });
